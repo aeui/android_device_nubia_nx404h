ea7910e2d11d0a3316c136b10180fd2149486c50
Sattarvoybek <sattarvoybek@gmail.com>
2017-07-15 13:00:47 +0300

    Revert "update"
    This reverts commit 85eec24dc421785e7094fd76c05be1dffd839a18.

--- NX404H.mk
+++ NX404H.mk
@@ -166,14 +166,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/etc/init.d/66country:system/etc/init.d/66country
     
 # OMX
-PRODUCT_PACKAGES += \
-    libc2dcolorconvert \
-    libdivxdrmdecrypt \
-    libOmxAacEnc \
-    libOmxAmrEnc \
     libOmxCore \
-    libOmxEvrcEnc \
-    libOmxQcelp13Enc \
     libOmxVdec \
     libOmxVenc \
     libstagefrighthw
