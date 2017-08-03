bc9b1baa92e1ca35d0a3086e5ff59a6f057b198a
ччЧЧчч <30278318+XNUBIA@users.noreply.github.com>
2017-08-03 18:32:53 +0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,6 @@
 
 
-add_lunch_combo lineage_NX404H-userdebug
-add_lunch_combo lineage_NX404H-eng
+add_lunch_combo omni_NX404H-userdebug
+add_lunch_combo omni_NX404H-eng
 
 sh device/NUBIA/NX404H/update-changelog.sh
